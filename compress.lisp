2113d7335601fbec68b0bf8d3452129722a360af
serjihsklovski <serjihsklovski@gmail.com>
2017-04-29 22:29:09 +0300

    str-compress

--- compress.lisp
+++ compress.lisp
@@ -1,7 +1,11 @@
 (defun str-get-head (str)
   (cond
     (
-      (eq (length str) 0)
+      (or
+        (eq (length str) 0)
+        (null str)
+      )
+
       nil
     )
 
@@ -15,7 +19,11 @@
 (defun str-get-tail (str)
   (cond
     (
-      (eq (length str) 0)
+      (or
+        (eq (length str) 0)
+        (null str)
+      )
+
       nil
     )
 
@@ -29,6 +37,7 @@
 (defun str-is-empty? (str)
   (string= str "")
 )
+
 
 (defun str-iterate (str)
   (let 
@@ -44,6 +53,56 @@
   )
 )
 
+
+(defun str-compress (str)
+  (let
+    (
+      (compressed nil)
+      (curch (str-get-head str))
+      (str-tail (str-get-tail str))
+    )
+
+    (defun str-compress-closure-1 ()
+      (let
+        (
+          (amount 1)
+        )
+
+        (defun str-compress-closure-2 ()
+          (let
+            (
+              (nextch (str-get-head str-tail))
+            )
+
+            (setf str-tail (str-get-tail str-tail))
+
+            (cond
+              ((null curch)
+                compressed
+              )
+
+              ((string= nextch curch)
+                (setf amount (1+ amount))
+                (str-compress-closure-2)
+              )
+
+              (t
+                (setf compressed (append compressed (cons (cons curch amount) nil)))  ; adding dotted pair: (string . number)
+                (setf curch nextch)
+                (str-compress-closure-1)
+              )
+            )
+          )
+        )
+
+        (str-compress-closure-2)
+      )
+    )
+
+    (str-compress-closure-1)
+  )
+)
+
 ;; tests
 (setq next-char (str-iterate "test"))
 
@@ -56,3 +115,6 @@
 
 (print (str-is-empty? "test"))  ; => NIL
 (print (str-is-empty? ""))      ; => T
+
+(print (str-compress "aaabbbbccccc"))   ; => (("a" . 3) ("b" . 4) ("c" . 5))
+(print (str-compress ""))               ; => NIL
